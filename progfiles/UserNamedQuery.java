a1a832d5c4d3a0f473a0de24216f4d760014a61a
zsborsay <zsborsay@yahoo.com>
2018-06-21 19:54:16 +0300

    Done

--- progfiles/UserNamedQuery.java
+++ progfiles/UserNamedQuery.java
@@ -1,8 +1,8 @@
 @Entity
 @Table(name = "user")
 @NamedQueries({
-	@NamedQuery(name = "User.findUsersLikeSearch", 
-        query = "SELECT u FROM User u WHERE CONCAT(first_name, ' ', last_name) 
+  @NamedQuery(name = "User.findUsersLikeSearch",
+    query = "SELECT u FROM User u WHERE CONCAT(first_name, ' ', last_name) 
         	LIKE :search ") })
 public class User extends BaseEntity {
 	...
--- progfiles/UserRepository.java
+++ progfiles/UserRepository.java
@@ -1,6 +1,6 @@
 @Repository
 public interface UserRepository extends CrudRepository<User, Long> {
-	Iterable<User> findUsersLikeSearch(@Param("search") String search) 
+  Iterable<User> findUsersLikeSearch(@Param("search") String search) 
     		throws RepositoryException;	
 	...
 }
